c1d26f5a696d207b6c7be902ce46687a18c9ca6d
royCo <64268522+el-ang@users.noreply.github.com>
2021-12-02 15:21:51 +0800

    jQuery Again

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function saveIcon(){
     e.each((i)=>{
         html2canvas($(e[i])[0], {backgroundColor: null}).then((c)=>{
             let l = c.toDataURL("image/png").replace("image/png", "image/octet-stream");
-            $(".rem").append($(`<a href="${l}"><b>Icon#${i}}</b></a>`));
+            $(".rem").append(`<a href="${l}"><b>Icon#${i}}</b></a>`);
         });
     });
 }
