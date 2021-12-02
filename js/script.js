0e53c59f4a0ce59ab0a530525a983218638dc6fd
royCo <64268522+el-ang@users.noreply.github.com>
2021-12-02 14:18:36 +0800

    Save Icon

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+function saveIcon(){
+    let e = $("i.fad.fa-10x");
+    e.forEach((i)=>{
+        html2canvas($(this), {backgroundColor: null}).then((c)=>{
+            let img = c.toDataURL("image/png").replace("image/png", "image/octet-stream");
+            $(".rem").append(`a[href=\"${img}\"]{Icon#${i}}`);
+        });
+    });
+}
+
 $(document).ready(()=>{
-    
+    saveIcon();
 });
