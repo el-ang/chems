49dbdaffc5dbf02a17d169300f98f3618eb4bee6
royCo <64268522+el-ang@users.noreply.github.com>
2021-12-03 09:33:39 +0800

    Config Chems

--- js/script.js
+++ js/script.js
@@ -1,13 +1,50 @@
-function saveIcon(){
-    let e = $("i.fad.fa-10x");
-    e.each((i)=>{
-        html2canvas($(e[i])[0], {backgroundColor: null}).then((c)=>{
-            let l = c.toDataURL("image/png").replace("image/png", "image/octet-stream");
-            $(".rem").append(`<a href="${l}"><b>Icon#${i}}</b></a>`);
-        });
-    });
+function makeJSON(){
+    let data = [];
+    for(let i = 0; i < 118; i++){
+        data[i] = {
+            id: {
+                z: (i+1),
+                sym: "",
+                m: ((i+1.1)*2),
+                pos: {
+                    group: "",
+                    period: Math.round((i+1)/16.85)
+                },
+                class: "",
+                cpkColor: "#null",
+                discovery: 1620
+            },
+            physic: {
+                phase: "",
+                melt: {
+                    kelv: 0,
+                    fahr: 0,
+                    celc: 0
+                },
+                boil: {
+                    kelv: 0,
+                    fahr: 0,
+                    celc: 0
+                },
+                density: 0
+            },
+            prop: {
+                electron: {
+                    config: ["1s^1"],
+                    neg: 0
+                },
+                radius: {
+                    cov: 0,
+                    vanDerWaals: 0
+                }
+            }
+        };
+    }
+    let url = URL.createObjectURL(new Blob(JSON.stringify(data), {type: "text/plain"}));
+    let a = $(`<a href="${url}" download="elements.json"></a>`);
+    a.click();
 }
 
 $(document).ready(()=>{
-    saveIcon();
+    makeJSON();
 });
